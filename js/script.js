5f6e0b701048e4e7a83a508c26461c30559f15f0
wmcqueen9 <42362967+wmcqueen9@users.noreply.github.com>
2022-10-19 16:03:58 -0500

    Accept and Vallidate Guess

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-const guessedLetters=document.querySelector(".guessed-letters");
+const guessedLettersElement=document.querySelector(".guessed-letters");
 const guessButton=document.querySelector(".guess");
 const guessLetter=document.querySelector(".letter");
 const progressWord=document.querySelector(".word-in-progress");
@@ -7,6 +7,7 @@ const span=document.querySelector(".remaining span");
 const message=document.querySelector(".message");
 const hiddenButton=document.querySelector(".play-again");
 const word="magonlia";
+const guessedLetters=[];
 
 const reveal=function(word){
     const revealletters=[]; // an array for the 8 circle symbols
@@ -20,7 +21,39 @@ reveal(word);
 
 guessButton.addEventListener("click", function(e){
 e.preventDefault();
-const capture=guessLetter.value;
-console.log(capture);
+
+message.innerText="";
+const myGuess=guessLetter.value;
+
+const test=letterInput(myGuess);
+
+if(test) {
+    makeGuess(myGuess);
+}
 guessLetter.value="";
 });
+
+const letterInput=function(input){
+    const acceptedLetter=/[a-zA-Z]/;
+    if(input.length===0) {
+        message.innerText= `Enter a letter`;}
+        else if (input.length>1){
+            message.innerText=`Please enter one letter`;
+        } else if(!input.match(acceptedLetter)){
+            message.innerText=`Please enter a word`;
+        } else{
+            return input;
+        }
+    
+    };
+
+      const makeGuess=function(myGuess){
+        myGuess=myGuess.toUpperCase();
+        if (guessedLetters.includes(myGuess)){
+            message.innerText="Made that Guess already";
+          
+        }else{
+            guessedLetters.push(myGuess);
+            console.log(guessedLetters);
+        }
+    };
