17ef49fc4716aa782b7d24aa304bcc1c9de667a0
wmcqueen9 <42362967+wmcqueen9@users.noreply.github.com>
2022-10-28 14:39:36 -0500

    Fetch Word

--- js/script.js
+++ js/script.js
@@ -2,13 +2,25 @@ const guessedLettersElement=document.querySelector(".guessed-letters");
 const guessButton=document.querySelector(".guess");
 const guessLetter=document.querySelector(".letter");
 const progressWord=document.querySelector(".word-in-progress");
-const remainingGuess=document.querySelector(".remaining");
+const remainingGuessElemwnt=document.querySelector(".remaining");
 const span=document.querySelector(".remaining span");
 const message=document.querySelector(".message");
 const hiddenButton=document.querySelector(".play-again");
-const word="magonlia";
+let word="magonlia";
 const guessedLetters=[];
+let remainingGuesses = 8;
 
+const getWord= async function(){
+    const res=await fetch("https://gist.githubusercontent.com/skillcrush-curriculum/7061f1d4d3d5bfe47efbfbcfe42bf57e/raw/5ffc447694486e7dea686f34a6c085ae371b43fe/words.txt");
+    const data=await res.text();
+    const wordArray = data.split("\n");
+   // console.log(wordArray);
+    const randomIndex = Math.floor(Math.random() * wordArray.length);
+    word = wordArray[randomIndex].trim();
+    reveal(word);
+
+};
+getWord();
 const reveal=function(word){
     const revealletters=[]; // an array for the 8 circle symbols
     for(const letter of word){
@@ -47,14 +59,15 @@ const letterInput=function(input){
     
     };
 
-      const makeGuess=function(myGuess){
-        myGuess=myGuess.toUpperCase();
-        if (guessedLetters.includes(myGuess)){
+      const makeGuess=function(guess){
+        guess=guess.toUpperCase();
+        if (guessedLetters.includes(guess)){ 
             message.innerText="Made that Guess already";
           
         }else{
-            guessedLetters.push(myGuess);
+            guessedLetters.push(guess);
             console.log(guessedLetters);
+            guessCount(guess);
             updateLetter();
             updateWord(guessedLetters);
         }
@@ -84,6 +97,26 @@ const letterInput=function(input){
        progressWord.innerText=revealWord.join("");
        checkWon();
     };
+    const guessCount=function(guess){
+       const upperWord= word.toUpperCase();
+        if(!upperWord.includes(guess)){
+            message.innerText=`This word does not have a ${guess}`;
+            remainingGuesses -= 1;
+        }else{
+            message.innerText=`Hey! This word does have a ${guess}`;
+        }
+
+
+        if(remainingGuesses===0){
+            message.innerText=`GAME OVER, sorry but the word was ${word}`;
+        }else if (remainingGuesses ===1){
+            span.innerText=`1 guess`
+        }
+        else{
+            span.innerText=`You have ${remainingGuesses} guess remaining`;
+        }
+    };
+
 const checkWon=function(){
     if(word.toUpperCase()==progressWord.innerText){
         message.classList.add("win");
